b3dad26cd6b4b1a2edb36dd3256603f28ab69de1
Maxim <kopernik25@yandex.ru>
2022-10-15 22:37:45 +0500

    Updates

---
+++ precache-manifest.c9bb6b23a9cf7c4ec370cc94701af691.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0359136adb05b09b0bc8915c36fed5a5",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "e64a8890ec0c737c894a",
+    "url": "/todo-react/static/css/main.3d731d5d.chunk.css"
+  },
+  {
+    "revision": "899712c8debbce5cb876",
+    "url": "/todo-react/static/js/2.a277081d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.a277081d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e64a8890ec0c737c894a",
+    "url": "/todo-react/static/js/main.7a9aca4f.chunk.js"
+  },
+  {
+    "revision": "1614377f578425b5d648",
+    "url": "/todo-react/static/js/runtime-main.92cd31f8.js"
+  }
+]);
